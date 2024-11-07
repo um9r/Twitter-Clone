8267cea079382dc2d57cca68255bde5348e930b1
Umar <faroukumar99@gmail.com>
2024-11-07 15:12:35 +0100

    comment modal

--- components/modals/CommentModal.js
+++ components/modals/CommentModal.js
@@ -7,8 +7,59 @@ export default function CommentModal() {
   const dispatch = useDispatch();
   return (
     <>
-      <Modal open={isOpen} onClose={() => dispatch(closeCommentModal())}>
-        <div className="w-[500px] h-[500px]">This is the comment modal</div>
+      <Modal
+        className="flex justify-center items-center"
+        open={isOpen}
+        onClose={() => dispatch(closeCommentModal())}
+      >
+        <div
+          className="w-full h-full
+        rounded-lg bg-black border border-gray-500 
+        sm:w-[600px] sm:h-[386px] text-white
+        sm:p-10 p-4"
+        >
+          <div>
+            <div className="flex space-x-3">
+              <img
+                className="w-12 h-12 object-cover rounded-full"
+                src="/assets/kylie.png"
+              />
+
+              <div>
+                <div className="flex space-x-1.5">
+                  <h1 className="font-bold">Kylie</h1>
+                  <h1 className="text-gray-500">@kylie</h1>
+                </div>
+                <p className="mt-1">This is awesome</p>
+                <h1 className="text-gray-500 text-[15px] mt-2">
+                  Replying to <span className="text-[#1b9bf0]">@farouk</span>
+                </h1>
+              </div>
+            </div>
+          </div>
+
+          <div className="mt-11">
+            <div className="flex space-x-3">
+              <img
+                className="w-12 h-12 object-cover rounded-full"
+                src="/assets/kylie.png"
+              />
+
+              <div className="w-full">
+                <textarea
+                  placeholder="Tweet your reply"
+                  className="w-full text-lg outline-none
+                bg-transparent resize-none
+                "
+                />
+                <div>
+                  <div></div>
+                  <button>Tweet</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </Modal>
     </>
   );
